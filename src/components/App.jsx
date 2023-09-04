7d26a61268ef970e2825fd5c646c333b480a5727
camariu <amariutei1998@gmail.com>
2023-09-04 11:49:01 +0300

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,10 @@ import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 export const App = () => {
-  const [contacts, setContacts] = useState();
+  const [contacts, setContacts] = useState([ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},]);
 
   useEffect(() => {
     const storagedData = JSON.parse(localStorage.getItem('storedData'));
@@ -22,7 +25,11 @@ export const App = () => {
 
   const addContacts = (name, number) => {
     const newContact = { id: nanoid(), name, number };
-    setContacts(prev => [...prev, newContact]);
+    if(contacts){
+      setContacts(prev => [...prev, newContact])
+    }else{
+      setContacts([newContact])
+    }
   };
 
   const handleFilterChange = value => {
